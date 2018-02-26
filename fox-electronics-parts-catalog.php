3893e7bceca116dd775e7622811771747d01d248
Michael Wender <michael@michaelwender.com>
2018-02-26 14:12:55 -0500

    First commit

---
+++ fox-electronics-parts-catalog.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Fox Electronics Parts Catalog
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Adds CPTs necessary to store the Fox Electronics Parts Catalog inside the WordPress database.
+ * Author:          Michael Wender
+ * Author URI:      https://mwender.com
+ * Text Domain:     proposal-manager
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Proposal_Manager
+ */
