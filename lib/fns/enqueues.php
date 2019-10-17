9753ae361cab77e21f555adf855c2c5e40206c18
Michael Wender <michael@michaelwender.com>
2019-10-17 10:35:19 -0400

    Updating DataTables 1.10.19 => 1.10.20

--- lib/fns/enqueues.php
+++ lib/fns/enqueues.php
@@ -36,7 +36,7 @@ function add_enqueues(){
   }
 
   // Register DataTables.js
-  wp_register_script( 'datatables', '//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js', ['jquery'], '1.10.19', true );
+  wp_register_script( 'datatables', '//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js', ['jquery'], '1.10.20', true );
   wp_register_script( 'datatables-init', plugin_dir_url( __FILE__ ) . '../js/datatables-init.js', ['datatables'], filemtime( plugin_dir_path( __FILE__ ) . '../js/datatables-init.js' ), true );
 
   // CSS
