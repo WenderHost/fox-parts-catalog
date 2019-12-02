bd010e0b9dc40cfd36bb774c252c646a4daf4af9
Michael Wender <michael@michaelwender.com>
2019-12-02 14:24:28 -0500

    Matching `a` as a part size

--- lib/fns/redirection.php
+++ lib/fns/redirection.php
@@ -5,7 +5,7 @@ namespace FoxParts\redirection;
 function crystals_redirect(){
   //\foxparts_error_log('REQUEST_URI = ' . $_SERVER['REQUEST_URI']);
   $request_uri = $_SERVER['REQUEST_URI'];
-  if( preg_match( '/foxpart\/FC([0-9])([a-z]{4})([a-z])([a-z])\/?([0-9]+\.[0-9]+)?/i', $request_uri, $matches ) ){
+  if( preg_match( '/foxpart\/FC([0-9a])([a-z]{4})([a-z])([a-z])\/?([0-9]+\.[0-9]+)?/i', $request_uri, $matches ) ){
     //\foxparts_error_log( print_r( $matches, true ) );
     $part_url = 'foxpart/FC' . $matches[1] . $matches[2] . '_' . $matches[4] . '/';
 
