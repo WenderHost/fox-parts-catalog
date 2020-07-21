4fa5c7b71a29addf7e86b3e68445b43e88c2dca4
Michael Wender <michael@michaelwender.com>
2020-07-21 11:56:11 -0400

    Adding Part Number to localized vars

--- lib/fns/shortcodes.php
+++ lib/fns/shortcodes.php
@@ -366,6 +366,7 @@ function part_series_results( $atts ){
   $part_number = \FoxParts\utilities\split_part_number( $args['s'] );
   $localized_vars = [];
   $localized_vars['partSeriesSearchUrl'] = get_site_url( '', '/wp-json/foxparts/v1/get_part_series?partnum=' . $args['s'] );
+  $localized_vars['partNumber'] = $part_number;
   $localized_vars['frequency'] = ( array_key_exists( 'frequency', $part_number ) )? $part_number['frequency'] : '' ;
   wp_localize_script( 'datatables-init', 'wpvars', $localized_vars );
   return '<table id="partseries"><thead><tr><th style="width: 40px;"></th><th style="width: 10%;">Part Series</th><th style="width: 10%;">Part Type</th><th style="width: 55%;">Details</th><th style="width: 20%;">Datasheet</th></tr></thead><tbody></tbody></table>';
